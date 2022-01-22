af68ec03b671e45e08760d61866f0c4dea2ad4cb
SamCoThePug <samcothepug@gmail.com>
2022-01-22 15:34:43 +0000

    Add gitignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const db = require('quick.db')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 var session = require('express-session')
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
@@ -12,7 +14,8 @@ app.use(session({
 app.listen(6060)
 
 app.post("/auth/login", async(req, res) => {
-
+  if(!req.body.username) return res.json({error:true,message:"Username is required"});
+  if(!req.body.password) return res.json({error:true,message:"Password is required"});
 })
 
 app.get("/login", async (req, res) => {
